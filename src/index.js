e3f32c1a9a499d3e0a866203266b004d45a9d2e6
Aron Strandberg <strandberg.aron@gmail.com>
2017-11-19 23:54:28 +0100

    add hot reloading

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { AppContainer } from 'react-hot-loader'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
 registerServiceWorker();
+
+const render = (Component) => {
+  ReactDOM.render((
+    <AppContainer>
+      <Component />
+    </AppContainer>
+    ), document.getElementById('root')
+  )
+}
+
+render(App)
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./components/App/App', () => { render(App) })
+}
