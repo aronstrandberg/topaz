b4bbbd71e6376325a3b8b69bce737734c2d9bfb8
Aron Strandberg <strandberg.aron@gmail.com>
2017-11-20 00:38:11 +0100

    Add a basic frontpage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from 'images/djubileet-logo.png';
 import css from './App.scss';
 import Typekit from 'react-typekit';
 
 class App extends Component {
   render() {
     return (
-      <div className={css.app}>
+      <main className={css.app}>
         <header className={css.header}>
           <img src={logo} className={css.logo} alt="logo" />
-          <h1 className={css.title}>Welcome to React</h1>
+          <h1 className={css.title}>
+            djubileet 2018
+          </h1>
         </header>
         <p className={css.intro}>
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Datasektionen fyller 35!
         </p>
         <Typekit kitId="xfh3gcv" />
-      </div>
+      </main>
     );
   }
 }
