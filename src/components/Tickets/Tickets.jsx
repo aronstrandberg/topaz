3a6ccbfdfe58e1fe18bf83695eeb166d1444e2aa
Aron Strandberg <aron@aronstrandberg.com>
2018-07-26 23:27:53 +0200

    Switch to images instead of iframes on Tickets page

--- src/components/Tickets/Tickets.jsx
+++ src/components/Tickets/Tickets.jsx
@@ -1,27 +1,42 @@
 import React from 'react'
 import css from './Tickets.scss'
+import slutfesten from 'images/slutfesten.png'
+import banketten from 'images/banketten.png'
 
 const Tickets = () => (
   <div className={css.tickets}>
     <h1 className={css.headline}>Biljetter</h1>
     <div className={css.section}>
       <h2>Slutfesten</h2>
+
+      <a href="http://slutfesten.djubileet.se">
+        <h4>Köp biljett här</h4>
+      </a>
       <div className={css.ticket}>
-        <iframe
-          className={css.iframe}
-          src="http://slutfesten.djubileet.se/"
-          title="slutfesten"
-        />
+        <a href="http://slutfesten.djubileet.se">
+          <img
+            className={css.img}
+            src={slutfesten}
+            alt=""
+            title="slutfesten"
+          />
+        </a>
       </div>
     </div>
     <div className={css.section}>
       <h2>Banketten</h2>
+      <a href="http://bankett.djubileet.se">
+        <h4>Köp biljett här</h4>
+      </a>
       <div className={css.ticket}>
-        <iframe
-          className={css.iframe}
-          src="http://bankett.djubileet.se/"
-          title="slutfesten"
-        />
+        <a href="http://bankett.djubileet.se">
+          <img
+            className={css.img}
+            src={banketten}
+            alt=""
+            title="banketten"
+          />
+        </a>
       </div>
     </div>
   </div>
