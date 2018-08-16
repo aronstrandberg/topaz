81b6ae3a964e62bc83c7b530fc505c1cf782e4fd
Aron Strandberg <aron@aronstrandberg.com>
2018-08-16 11:07:37 +0200

    Add new events to schedule
    Also move data to separate file

--- src/components/Schedule/Schedule.jsx
+++ src/components/Schedule/Schedule.jsx
@@ -2,38 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import moment from 'moment'
 import classnames from 'classnames'
+import schedule from './data'
 import css from './Schedule.scss'
 
-const parse = (date) => moment(date, 'YYYY-MM-DD')
-const format = (date) => moment(date).format('DD MMM')
-
-const schedule = [
-  {
-    id: 1,
-    date: parse('2018-04-7'),
-    title: 'Halvårsfesten',
-  },
-  {
-    id: 2,
-    date: parse('2018-09-29'),
-    title: 'Banketten',
-    facebook: 'https://www.facebook.com/events/218477885405600/',
-    info: { __html: "<b>Biljetter</b><br/>Den andra omgången biljetter släpps onsdag 4/7, klockan 19.00 på <a href='http://bankett.djubileet.se'>http://bankett.djubileet.se</a>" },
-  },
-  {
-    id: 3,
-    date: parse('2018-10-03'),
-    title: 'Onsdagspub XXL',
-    featuring: 'DKM',
-  },
-  {
-    id: 4,
-    date: parse('2018-10-06'),
-    title: 'Slutfesten',
-    facebook: 'https://www.facebook.com/events/915084615341399/',
-    info: { __html: "<b>Biljetter</b><br/>Den andra omgången biljetter släpps 12/7 på <a href='http://slutfesten.djubileet.se'>http://slutfesten.djubileet.se</a>" },
-  },
-]
+const format = (date) => moment(date, 'YYYY-MM-DD').format('DD MMM')
 
 const Schedule = () => (
   <div className={css.wrapper}>
---
+++ src/components/Schedule/data.js
@@ -0,0 +1,53 @@
+const schedule = [
+  {
+    id: 1,
+    date: '2018-04-7',
+    title: 'Halvårsfesten',
+  },
+  {
+    id: 2,
+    date: '2018-09-29',
+    title: 'Banketten',
+    facebook: 'https://www.facebook.com/events/218477885405600/',
+    info: { __html: '<b>Biljetter</b><br/>Den andra omgången biljetter släpps onsdag 4/7, klockan 19.00 på <a href="http://bankett.djubileet.se">http://bankett.djubileet.se</a>' },
+  },
+  {
+    id: 3,
+    date: '2018-09-30',
+    title: 'Bakis med BAKIS',
+    featuring: 'Baknämnden',
+  },
+  {
+    id: 4,
+    date: '2018-09-30',
+    title: 'Improvisationskväll',
+    featuring: 'MetaSpexet',
+  },
+  // {
+  //   id: 5,
+  //   date: '2018-10-02',
+  //   title: 'Lunchföreläsning med Facebook',
+  //   featuring: 'NLG',
+  // },
+  {
+    id: 6,
+    date: '2018-10-03',
+    title: 'Onsdagspub XXL',
+    featuring: 'DKM',
+  },
+  {
+    id: 7,
+    date: '2018-10-05',
+    title: 'SyPlunta',
+    featuring: 'Prylmångleriet',
+  },
+  {
+    id: 8,
+    date: '2018-10-06',
+    title: 'Slutfesten',
+    facebook: 'https://www.facebook.com/events/915084615341399/',
+    info: { __html: '<b>Biljetter</b><br/>Den andra omgången biljetter släpps 12/7 på <a href="http://slutfesten.djubileet.se">http://slutfesten.djubileet.se</a>' },
+  },
+]
+
+export default schedule
